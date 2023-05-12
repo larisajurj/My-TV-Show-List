a270e931ac979d75cd40de22f88a9a9fef9c9a68
Larisa Jurj <larisajurj21@gmail.com>
2023-05-12 16:56:52 +0300

    added login scene

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,33 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+public class Main extends Application{
+    Stage window;
+    Scene startingScene;
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //here will be the main javafx code
+        try {
+            window = primaryStage;
+            Parent layout1 = FXMLLoader.load(getClass().getClassLoader().getResource("view/LoginScene.fxml"));
+            startingScene = new Scene(layout1);
+            String css = this.getClass().getClassLoader().getResource("css/Style.css").toExternalForm();
+            startingScene.getStylesheets().add(css);
+            window.setScene(startingScene);
+            window.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
