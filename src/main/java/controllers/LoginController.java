244d0ccd081dbefce1b98d3a34b6730c9df9480e
Eric Florea <eric.florea@student.upt.ro>
2023-05-24 15:35:32 +0300

    "finished story MTSL-16"

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import utilities.MySqlConnect;
@@ -23,6 +24,8 @@ public class LoginController {
     private Stage stage;
     private Scene scene;
     private Parent layout;
+    @FXML
+    private Label userLabel;
 
     public void LoginSuccessfully(ActionEvent event) {
         String enteredUsername = UsernameField.getText();
@@ -48,6 +51,13 @@ public class LoginController {
                     e.printStackTrace();
                 }
                 return; // Exit the method if the login is successful
+            }
+            else if(!user.getUsername().equals(enteredUsername)){
+                userLabel.setText("Username is wrong!");
+            } else {
+                if(!user.getPassword().equals(enteredPassword)) {
+                    userLabel.setText("Password is wrong!");
+                }
             }
         }
 
