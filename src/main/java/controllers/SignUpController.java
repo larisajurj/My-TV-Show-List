0ad5a13a47b42292e0414b0b8aced6d06e0b82c3
Eric Florea <eric.florea@student.upt.ro>
2023-05-27 00:57:11 +0300

    "Ended MTSL-19. Added Profile Pictures"

--- src/main/java/controllers/SignUpController.java
+++ src/main/java/controllers/SignUpController.java
@@ -25,16 +25,19 @@ public class SignUpController {
     public TextField UsernameField;
     public TextField PasswordField;
     public TextField FavoriteQuoteField;
+    public TextField ConfirmPasswordField;
     private Stage stage;
     private Scene scene;
     private Parent layout;
     @FXML
     private Label accountLabel;
     private EncryptPassword encryptPassword = new EncryptPassword();
+    private String profilePicture = "1";
 
     public void createAccount(ActionEvent event) {
         String enteredUsername = UsernameField.getText();
         String enteredPassword = PasswordField.getText();
+        String confirmPassword = ConfirmPasswordField.getText();
         String enteredFavQuote = FavoriteQuoteField.getText();
         String encryptedPassword = encryptPassword.encrypt(enteredPassword);
 
@@ -44,22 +47,30 @@ public class SignUpController {
         for(UserNameList user : userList) {
             if(user.getUsername().equals(enteredUsername)) {
                 accountLabel.setText("Username is taken!");
+                return;
             }
             else {
-                try {
-                    Connection conn = MySqlConnect.ConnectDb();
-                    PreparedStatement ps = conn.prepareStatement("INSERT INTO user_table (user, password, favQuote, profilePic) VALUES (?, ?, ?, 1)");
-                    ps.setString(1, enteredUsername);
-                    ps.setString(2, encryptedPassword);
-                    ps.setString(3, enteredFavQuote);
-                    ps.executeUpdate();
-                    System.out.println(encryptedPassword);
+                if(!enteredPassword.equals(confirmPassword)) {
+                    accountLabel.setText("Passwords are not the same. Try again!");
+                    return;
+                }
+                else {
+                    try {
+                        Connection conn = MySqlConnect.ConnectDb();
+                        PreparedStatement ps = conn.prepareStatement("INSERT INTO user_table (user, password, favQuote, profilePic) VALUES (?, ?, ?, ?)");
+                        ps.setString(1, enteredUsername);
+                        ps.setString(2, encryptedPassword);
+                        ps.setString(3, enteredFavQuote);
+                        ps.setString(4, Integer.valueOf(profilePicture).toString());
+                        ps.executeUpdate();
+                        System.out.println(encryptedPassword);
 
 
-                    accountLabel.setText("Account created! Go back and log in.");
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                    System.out.println(sqlException.getMessage());
+                        accountLabel.setText("Account created! Go back and log in.");
+                    } catch (SQLException sqlException) {
+                        sqlException.printStackTrace();
+                        System.out.println(sqlException.getMessage());
+                    }
                 }
             }
         }
@@ -77,5 +88,23 @@ public class SignUpController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public void profileOne(ActionEvent event) {
+        profilePicture = "1";
+    }
+    public void profileTwo(ActionEvent event) {
+        profilePicture = "2";
+    }
+    public void profileThree(ActionEvent event) {
+        profilePicture = "3";
+    }
+    public void profileFour(ActionEvent event) {
+        profilePicture = "4";
+    }
+    public void profileFive(ActionEvent event) {
+        profilePicture = "5";
+    }
+    public void profileSix(ActionEvent event) {
+        profilePicture = "6";
     }
 }
